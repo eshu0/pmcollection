6a72401de6d3c414130e524047987dc13bb789e7
eshu0 <willsawesometoolkit@gmail.com>
2021-01-16 17:38:01 +0000

    Update manager.go

--- pkg/manager.go
+++ pkg/manager.go
@@ -9,9 +9,46 @@ type PostmanManager struct {
 	Collections []*Collection
 }
 
-func (pm *PostmanManager) Load(filepath string) {
+func (pm *PostmanManager) SaveColletion(filepath string) {
 
-	pmcol := NewCollection("todo")
-	return pmcol.Load(filepath, pm.Logger)
+	for _, col := range pm.Collections {
+		success := col.Save(filepath, pm.Log)
 
+		if success {
+			pm.LogInfo("SaveColletion", "Saved "+col.Info.Name)
+		}
+	}
+}
+
+func (pm *PostmanManager) LoadColletion(filepath string) {
+
+	pmcol := NewCollection("somename")
+	cols := pm.Collections
+
+	pm.LogDebug("LoadColletion", "Started")
+	col, success := pmcol.Load(filepath, pm.Log)
+
+	if success {
+		pm.LogInfo("LoadColletion", "Loaded "+col.Info.Name)
+		cols = append(cols, col)
+	}
+	pm.Collections = cols
+}
+
+func (pm *PostmanManager) PrintCollections() {
+
+	pm.LogInfo("PrintCollections", "Started printing collections")
+
+	for _, col := range pm.Collections {
+
+		if col.Info != nil {
+			pm.LogInfo("PrintCollections", col.Info.Name)
+			pm.LogInfo("PrintCollections", col.Info.Postman_Id)
+			pm.LogInfo("PrintCollections", col.Info.Schema)
+		}
+
+		for _, item := range col.Item {
+			pm.LogInfo("PrintCollections", item.Name)
+		}
+	}
 }
