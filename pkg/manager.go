440273e660fbdebfb33c784a951442f33d281516
eshu0 <willsawesometoolkit@gmail.com>
2021-01-20 07:55:25 +0000

    Added new functions

--- pkg/manager.go
+++ pkg/manager.go
@@ -1,12 +1,29 @@
 package pmcollection
 
 import (
+	"fmt"
+
 	sl "github.com/eshu0/simplelogger/pkg"
 )
 
 type PostmanManager struct {
 	sl.AppLogger
-	Collections []*Collection
+	Collections map[string]*Collection
+	//Folderpath  string
+}
+
+func NewPostmanManager() *PostmanManager {
+	pm := PostmanManager{}
+	collections := make(map[string]*Collection)
+	pm.Collections = collections
+	return &pm
+}
+
+func (pm *PostmanManager) SaveColletions(folderpath string) {
+	for k, _ := range pm.Collections {
+		pm.SaveColletion(fmt.Sprintf("%s%s.postman_collection.json", folderpath, pm.Collections[k].Info.Name))
+	}
+
 }
 
 func (pm *PostmanManager) SaveColletion(filepath string) {
@@ -20,19 +37,50 @@ func (pm *PostmanManager) SaveColletion(filepath string) {
 	}
 }
 
+func (pm *PostmanManager) AddItem(collectionname string, item *Item) {
+
+	if pm.Collections[collectionname] == nil {
+		pm.LogDebug("AddItem", "Adding "+collectionname)
+		col := NewCollection(collectionname)
+		pm.AddColletion(col)
+	}
+
+	items := pm.Collections[collectionname].Item
+	items = append(items, item)
+	pm.Collections[collectionname].Item = items
+}
+
+func (pm *PostmanManager) AddColletion(col *Collection) {
+
+	if pm.Collections[col.Info.Name] != nil {
+		pm.LogDebug("AddColletion", "Updating "+col.Info.Name)
+		pm.Collections[col.Info.Name] = col
+
+	} else {
+		/*
+			cols := pm.Collections
+
+			pm.LogDebug("AddColletion", "Adding "+col.Info.Name)
+			cols = append(cols, col)
+			pm.Collections = cols
+		*/
+		pm.LogDebug("AddColletion", "Adding "+col.Info.Name)
+
+		pm.Collections[col.Info.Name] = col
+	}
+
+}
+
 func (pm *PostmanManager) LoadColletion(filepath string) {
 
 	pmcol := NewCollection("somename")
-	cols := pm.Collections
-
 	pm.LogDebug("LoadColletion", "Started")
 	col, success := pmcol.Load(filepath, pm.Log)
 
 	if success {
 		pm.LogInfo("LoadColletion", "Loaded "+col.Info.Name)
-		cols = append(cols, col)
+		pm.AddColletion(col)
 	}
-	pm.Collections = cols
 }
 
 func (pm *PostmanManager) PrintCollections() {
@@ -40,7 +88,6 @@ func (pm *PostmanManager) PrintCollections() {
 	pm.LogInfo("PrintCollections", "Started printing collections")
 
 	for _, col := range pm.Collections {
-
 		if col.Info != nil {
 			pm.LogInfo("PrintCollections", col.Info.Name)
 			pm.LogInfo("PrintCollections", col.Info.Postman_Id)
